0c017f81b5ffaaad42a2c974c54338e6048cbadc
Peter Allan David <linusclyde@gmail.com>
2014-08-27 00:03:43 -0800

    Add angular and material design libraries

---
+++ public/angular/scripts/MainMenu/controllers/MainMenuController.js
@@ -0,0 +1,11 @@
+mainMenuModule.controller('mainMenuController',['$scope', function($scope) {
+	
+	var tabs = [
+      { title: 'I AM A PHOTOGRAPHER', active: true,  disabled: false, content:"todo profile page of photographer" },
+      { title: 'I AM LOOKING FOR A PHOTOGRAPHER', active: false, disabled: true , content:"todo list of photographers" },
+    ];
+
+    $scope.activeIndex = 0;
+    $scope.tabs = [].concat(tabs);
+
+ }]);
