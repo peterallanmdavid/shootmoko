233a488fb5f904d264c9dc31eea1e74d81aac8ec
Peter Allan David <linusclyde@gmail.com>
2014-08-28 02:42:21 -0800

    added container for Photographer's profile and Portfolio List, D na ako maka2log

--- public/angular/scripts/MainMenu/controllers/MainMenuController.js
+++ public/angular/scripts/MainMenu/controllers/MainMenuController.js
@@ -1,14 +1,62 @@
 mainMenuModule.controller('mainMenuController',['$scope', function($scope) {
 	
 	var tabs = [
-      { title: 'I AM A PHOTOGRAPHER', active: true,  disabled: false, content:"todo profile page of photographer" },
-      { title: 'I AM LOOKING FOR A PHOTOGRAPHER', active: false, disabled: true , content:"todo list of photographers" },
+      {id:1, title: 'I AM A PHOTOGRAPHER', active: true,  disabled: false, content:"todo profile page of photographer" },
+      {id:2, title: 'I AM LOOKING FOR A PHOTOGRAPHER', active: false, disabled: true , content:"todo list of photographers" },
     ];
 
     $scope.activeIndex = 0;
     $scope.tabs = [].concat(tabs);
-
     $scope.showProfile = false;
-    $scope.showportfolio = false;
+    $scope.showPortfolio = false;
+
+
+    $scope.onTabSelected = function(tab){
+    	if (tab==1){
+    		$scope.showProfile = true;
+    		$scope.showPortfolio = false;
+    	}else
+    	{
+    		$scope.showProfile = false;
+    		$scope.showPortfolio = true;
+    	}
+    	
+    }
+
+
+
+    $scope.todos = [
+      {
+        face : '/images/portfolio_list/peter.jpg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/images/portfolio_list/emerson.jpg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/images/portfolio_list/frietz.jpg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/images/portfolio_list/james.jpg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+    ]
+
+
+
 
  }]);
--- public/angular/scripts/MainMenu/directives/MainMenuDirective.js
+++ public/angular/scripts/MainMenu/directives/MainMenuDirective.js
@@ -2,17 +2,35 @@ mainMenuModule.directive('mainMenuDirective', function() {
     return {
       	template: 
     		'<material-tabs selected="activeIndex" style="border: 1px solid #CCC; border-radius: 4px;">'+
-            '<material-tab ng-repeat="tab in tabs" onselected>'+
-                '<span>{{tab.title}}</span>'+
+            '<material-tab ng-repeat="tab in tabs" on-select="onTabSelected(tab.id)">'+
+                '{{tab.title}}'+
             '</material-tab>'+
         '</material-tabs>'+
 
         '<div class = "profile-container" ng-show = "showProfile">'+
-          'todo Profile Container here'+
+          '<material-card>'+
+            '<img src="/images/profile.jpg" class="material-card-image">'+
+            '<h2>MyProfile</h2>'+
+             '<p>TodoProfile Here churva ek ek, sample lang to para may laman ang profile container</p>'+
+          '</material-card>'+
+
         '</div>'+
 
-        '<div class = "portfolio-container" ng-show = "showPortfolio">'+
-          'todo list of Portfolio here'+
+        '<div class = "portfolio-container" ng-show ="showPortfolio">'+
+        '<material-list>'+
+          '<material-item ng-repeat="item in todos">'+
+            '<div class="material-tile-left">'+
+                '<img ng-src="{{item.face}}" class="face">'+
+            '</div>'+
+            '<div class="material-tile-content">'+
+              '<h2>{{item.what}}</h2>'+
+              '<h3>{{item.who}}</h3>'+
+              '<p>'+
+                '{{item.notes}}'+
+              '</p>'+
+            '</div>'+
+          '</material-item>'+
+        '</material-list>'+
         '</div>'
     };
   });
