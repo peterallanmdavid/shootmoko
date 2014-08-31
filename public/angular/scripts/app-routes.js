73a07a3997323b6afe7d9b9ef751fda6689a99f9
Aldee Paul <aldeepaul@gmail.com>
2014-08-31 21:29:55 +0800

    some additional potential links/data on users home

--- public/angular/scripts/app-routes.js
+++ public/angular/scripts/app-routes.js
@@ -1,33 +1,11 @@
 mainModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/',
       {
-        //templateUrl: "/angular/scripts/main-view.html",
 		templateUrl: "/angular/views/home.html",
         controller: "AppCtrl"
       }
-    )
+    );
     
-    $routeProvider.when('/editprofile',
-      {
-        templateUrl: "/angular/scripts/edit-profile.html",
-        controller: "AppCtrl"
-      }
-    )
-
-        $routeProvider.when('/mystudio',
-      {
-        templateUrl: "/angular/scripts/my-studio.html",
-        controller: "AppCtrl"
-      }
-    )
-
-    $routeProvider.when('/myclients',
-      {
-        templateUrl: "/angular/scripts/my-clients.html",
-        controller: "AppCtrl"
-      }
-    )
-
     $routeProvider.when('/photographerhome',
       {
 		templateUrl: "/angular/views/photographer.html",
@@ -44,6 +22,7 @@ mainModule.config(['$routeProvider', function($routeProvider) {
 
 }])
 
+//@todo move this to proper location
 mainModule.controller("AppCtrl", function($scope) {
     $scope.model = {
         message: "This is my app!!!"
