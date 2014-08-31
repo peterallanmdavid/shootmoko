c0e6befa5e91403200a531af918ff0ed02136a9f
Aldee Paul <aldeepaul@gmail.com>
2014-09-01 00:17:55 +0800

    removed hash and salt from return

--- models/account.js
+++ models/account.js
@@ -2,12 +2,26 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
 
+
 var Account = new Schema({
     username: String,
     password: String,
 	role: String,
 	usertype: String
 });
+
+
+Account.options.toJSON = {
+	    transform: function(doc, ret, options) {
+	        ret.id = ret._id;
+	        delete ret._id;
+	        delete ret.__v;
+			delete ret.salt;
+			delete ret.hash;
+	        return ret;
+	    }
+};
+
 
 Account.plugin(passportLocalMongoose);
 
