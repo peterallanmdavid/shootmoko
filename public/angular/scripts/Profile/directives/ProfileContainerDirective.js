0d8e1e0c837797318baebe12129f37ed4da4dff1
Peter Allan David <linusclyde@gmail.com>
2014-08-31 16:38:17 -0800

    may inaus lang

--- public/angular/scripts/Profile/directives/ProfileContainerDirective.js
+++ public/angular/scripts/Profile/directives/ProfileContainerDirective.js
@@ -1,24 +1,36 @@
 profileModule.directive('profileContainer', function() {
     return {
       	template: 
-	        '<material-card>'+  	
-			    '<material-sidenav class="material-sidenav-left material-whiteframe-z2" component-id="left">'+
-			      '<material-toolbar class="material-theme-light">'+
-			      		'<h1>My Profile</h1>'+
-			      '</material-toolbar>'+
-			      '<material-content class="material-content-padding">'+
+     
+      		'<div class = "profile-option-container col-md-4 col-sm-4 col-xs-4">'+
+	        '<material-card class="profile-option">'+  	
+	          
+	         	'<material-toolbar class="material-theme-light">'+
+			      		'My Profile'+
+			     '</material-toolbar>'+
+	             '<material-content class="material-content-padding">'+
 		            '<material-whiteframe class="material-whiteframe-z1" layout layout-align="center center">'+    
-						'<material-button ng-click="goto(\'/editprofile\')">Edit Profile</material-button>'+    
+						'<material-button ng-click="goto(\'/editprofile\')">Edit Page</material-button>'+    
 					'</material-whiteframe>'+    
 					'<material-whiteframe class="material-whiteframe-z1" layout layout-align="center center">'+    
 						'<material-button ng-click="goto(\'/mystudio\')">My Studio</material-button>'+    
 					'</material-whiteframe>'+  
 					'<material-whiteframe class="material-whiteframe-z1" layout layout-align="center center">'+    
-						'<material-button ng-click="goto(\'/myclients\')">MClients</material-button>'+    
+						'<material-button ng-click="goto(\'/myclients\')">My Clients</material-button>'+    
 					'</material-whiteframe>'+  	
-			      '</material-content>'+
-			    '</material-sidenav>'+      	          
-	            //'<img src="/images/profile.jpg" class="material-card-image-profile">'+		
-	        '</material-card>'
+			      '</material-content>'+	
+	        '</material-card>'+
+	        '</div>'+
+
+	        '<div class = "profile-content-container col-md-8 col-sm-8 col-xs-8">'+
+	        '<material-card class="profile-content">'+
+	        	'<img src="/images/profile.jpg" class="material-card-image-profile">'+	
+ 				'<h2>MyProfile</h2>'+
+      			'<p>'+
+      				'This will contain Profile Details'+
+      			'</p>'+
+	        '</material-card>'+
+	        '</div>'
+
     };
   });
