bcde2ba5c730133aebedeff1e33e339f2d088682
peterallanmdavid <linusclyde@gmail.com>
2014-09-05 20:10:46 +0800

    minor edit on photog list

--- public/angular/scripts/photographer/controllers/photographerController.js
+++ public/angular/scripts/photographer/controllers/photographerController.js
@@ -3,13 +3,21 @@ photographerModule.controller('photographerController',['$scope', function($scop
 		{
 			title: "I am looking for a wedding photographer",
 			location: "Quezon City",
-			timePosted: "Fri Sep 05 2014 18:24:18 GMT+0800 (China Standard Time)"
+			timePosted: "Fri Sep 05 2014 18:24:18",
+			face : '/images/portfolio_list/pete.jpg',
 		},
 		{
 			title: "I am looking for a birthday photographer",
 			location: "Malabon City",
-			timePosted: "Fri Sep 05 2014 18:24:18 GMT+0800 (China Standard Time)"
-		},	
+			timePosted: "Fri Sep 05 2014 18:24:18",
+			face : '/images/portfolio_list/emerson.jpg',
+		},
+		{
+			title: "I am looking for a photographer",
+			location: "Cavite City",
+			timePosted: "Fri Sep 05 2014 18:24:18",
+			face : '/images/portfolio_list/frietz.jpg',
+		}		
 	];
 
 }]);
--- public/angular/scripts/photographer/directives/photogPostsContainer.js
+++ public/angular/scripts/photographer/directives/photogPostsContainer.js
@@ -1,10 +1,16 @@
 photographerModule.directive('photogPostsContainer', function() {
     return {
       	template: 
+
       	'<div class = "photog-posts-container" ng-repeat = "posts in postList">'+
+      		'<div class = "col-md-1 col-lg-1 col-sm-1">'+
+      			'<img ng-src="{{posts.face}}" class="face">'+
+      		'</div>'+
+      		'<div class = "post-detail col-md-11 col-lg-11 col-sm-11">'+
       		'<a href="#">{{posts.title}}</a>'+
-      		'<p>{{posts.location}}</p>'+
+      		'<p>Location: {{posts.location}}</p>'+
       		'<p>{{posts.timePosted}}</p>'+
+      		'</div>'+
       	'</div>'
     };
   });
