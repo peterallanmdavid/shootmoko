ca52958ddb561e4c2dc81abfcb5371ee559eb84b
peterallanmdavid <linusclyde@gmail.com>
2014-09-10 18:21:05 +0800

    minor change

--- public/angular/scripts/client/directives/clientHeaderContainer.js
+++ public/angular/scripts/client/directives/clientHeaderContainer.js
@@ -2,7 +2,7 @@ clientModule.directive('clientHeaderContainer', function() {
     return {
       	template: 
             '<div class= "client-header-container" ng-controller="clientController">'+
-              '<div class="row">'+
+              '<div>'+
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt1" ng-click="toggleHeader(1)">Add Post</button>'+
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt2" ng-click="toggleHeader(2)">List of Photographers</button>'+
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt3" ng-click="toggleHeader(3)">Specialty</button>'+ 
