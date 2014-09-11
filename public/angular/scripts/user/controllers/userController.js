8387e54bddac50ef1b755dbb8ce31784b81e282c
aldeepaul@gmail.com <Amorphis3>
2014-09-11 16:07:26 +0800

    basic register now saves to DB

--- public/angular/scripts/user/controllers/userController.js
+++ public/angular/scripts/user/controllers/userController.js
@@ -1,3 +1,22 @@
-userModule.controller('userController',['$scope', function($scope) {	
+userModule.controller('userController',['$scope', '$http', function($scope, $http) {	
 	$scope.msg = "(this msg is from userController)";
+
+	$scope.register = function(){
+		console.log($scope.user);
+		//move to a service
+		var postdata = {
+			username: $scope.user.username,
+			password: $scope.user.password,
+			usertype: $scope.user.usertype
+		};
+		$http({
+	        url: 'http://localhost:3000/api/register',
+	        method: "POST",
+	        data: postdata
+		}).success(function (response) {
+        	console.log(response);
+		}).error(function (errorResponse) {
+			console.log(errorResponse);
+		});
+	};
 }]);
