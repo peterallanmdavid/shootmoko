f2eec284cda4e8e319028f14aee54cff8f066f03
peterallanmdavid <linusclyde@gmail.com>
2014-09-12 16:02:28 +0800

    testing commit

--- public/angular/scripts/client/directives/clientHeaderContainer.js
+++ public/angular/scripts/client/directives/clientHeaderContainer.js
@@ -2,12 +2,13 @@ clientModule.directive('clientHeaderContainer', function() {
     return {
       	template: 
             '<div class= "client-header-container" ng-controller="clientController">'+
-              '<div>'+
+/*              '<div>'+
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt1" ng-click="toggleHeader(1)">Add Post</button>'+
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt2" ng-click="toggleHeader(2)">List of Photographers</button>'+
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt3" ng-click="toggleHeader(3)">Specialty</button>'+ 
                   '<button type="button" class="btn btn-success col-lg-3 col-md-3 col-sm-3 col-xs-3" ng-class="btn.bt4" ng-click="toggleHeader(4)">OpenShoots</button>'+
               '</div>'+
+*/
             '</div>'
     };
   });
