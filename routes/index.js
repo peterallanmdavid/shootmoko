e2b97dc163f98bdb30e791a51bcd2564792a0d95
aldeepaul@gmail.com <Amorphis3>
2014-11-17 14:58:11 +0800

    initial react stuffs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/react', function(req, res) {
+  res.render('index_react', { title: 'React Index' });
+});
+
 module.exports = router;
