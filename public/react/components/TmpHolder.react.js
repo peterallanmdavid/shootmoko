6f794452ebf16d6a4be9f796342e40885c19656c
aldeepaul@gmail.com <Amorphis3>
2014-11-17 16:08:22 +0800

    users list sample

--- public/react/components/TmpHolder.react.js
+++ public/react/components/TmpHolder.react.js
@@ -1,3 +1,42 @@
+var userList = [
+  {
+    "username": "aaaaa",
+    "role": "user",
+    "usertype": "photographer",
+    "photos": [],
+    "id": "541272c45217a0d027486f3a"
+  },
+  {
+    "username": "alaa",
+    "role": "user",
+    "usertype": "photographer",
+    "photos": [],
+    "id": "541272efbb32b72036858bfc"
+  },
+  {
+    "username": "aldeepaul",
+    "role": "user",
+    "usertype": "photographer",
+    "photos": [],
+    "id": "5412745e0ae60748304f05d4"
+  },
+  {
+    "username": "aldee1",
+    "role": "user",
+    "usertype": "photographer",
+    "photos": [],
+    "id": "5412b506d99e101022444230"
+  },
+  {
+    "username": "test1",
+    "role": "user",
+    "usertype": "photographer",
+    "photos": [],
+    "id": "542a769c891e72e82a6bcdd3"
+  }
+];
+
+
 var TmpHolder = React.createClass({  
   render: function() {
     return (
@@ -5,6 +44,7 @@ var TmpHolder = React.createClass({
         <h1>Temp Component Holder</h1>
         <LoginForm />
         <RegistrationForm />        
+        <UsersList />
       </div>
     );
   }
---
+++ public/react/components/UsersList.react.js
@@ -0,0 +1,9 @@
+var UsersList = React.createClass({    
+  render: function() {
+    return (      
+      <div className="usersList">
+        <h3>Users List</h3>
+      </div>
+    );
+  }
+});
