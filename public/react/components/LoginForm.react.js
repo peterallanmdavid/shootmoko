3c433aecf9e0e2cacc5316a7a0f1c3878cc55a30
aldeepaul@gmail.com <Amorphis3>
2014-12-04 13:43:52 +0800

    login form

--- public/react/components/LoginForm.react.js
+++ public/react/components/LoginForm.react.js
@@ -3,13 +3,21 @@ var LoginForm = React.createClass({
     e.preventDefault();    
   },
   render: function() {
-    return (      
-      <form className="loginForm" onSubmit={this.handleSubmit}>
-        <h3>LoginForm</h3>
-        <input type="text" placeholder="Username" ref="username" />
-        <input type="password" placeholder="Password" ref="password" />
-        <input type="submit" value="Login" />
-      </form>
+    return (            
+        <form className="loginForm form-signin" onSubmit={this.handleSubmit} role="form">
+          <h2 className="form-signin-heading">Please sign in</h2>
+          <label for="inputEmail" className="sr-only">Email address</label>
+          <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+          <label for="inputPassword" className="sr-only">Password</label>
+          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+          <span>New user? Click <Link to="register">HERE</Link> to register</span>
+        </form>      
     );
   }
 });
