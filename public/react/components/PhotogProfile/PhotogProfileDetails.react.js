369ab78df53835e2b84e77336c03d43b944e27c4
peterallanmdavid <linusclyde@gmail.com>
2014-12-21 01:47:29 -0800

    minor

--- public/react/components/PhotogProfile/PhotogProfileDetails.react.js
+++ public/react/components/PhotogProfile/PhotogProfileDetails.react.js
@@ -2,7 +2,7 @@ var PhotogProfileDetails = React.createClass({
 	render: function(){
 		return(
 				<div>
-					<div className = "col-md-5 col-lg-5" >
+					<div className = "col-md-6 col-lg-6" >
 						<img className = "photogProfilePicture" src="/images/portfolio_list/pete.jpg" alt="..."/>
 						<div className="photogProfileAction list-group">
 						  	<a href="#" className="list-group-item">Edit Profile</a>
@@ -11,7 +11,7 @@ var PhotogProfileDetails = React.createClass({
 						  	<a href="#" className="list-group-item">Booking Requests</a>
 						</div>
 					</div>
-					<div className = "col-md-7 col-lg-7" >
+					<div className = "col-md-6 col-lg-6" >
 
 			        <h4>{this.props.uDetails.name}</h4>
 			        <p>Level: {this.props.uDetails.level}</p>	
